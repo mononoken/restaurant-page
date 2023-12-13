b038c79c93211aff3dd91bf60be11d035a393ba7
mononoken <ken.maeshima@pm.me>
2023-12-13 02:53:05 -0800

    Setup webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
-import * as path from "path";
+const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
